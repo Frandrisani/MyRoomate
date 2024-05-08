ecbee1b1857d5b58bbf603cb4e5b5801b7b7f436
Francesco Andrisani <fra.andrisani01@gmail.com>
2024-05-08 10:16:54 +0100

    Initial commit

---
+++ src/main/java/backend_myroommate/MyRoomate/MyRoomateApplication.java
@@ -0,0 +1,13 @@
+package backend_myroommate.MyRoomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRoomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRoomateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend_myroommate/MyRoomate/MyRoomateApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_myroommate.MyRoomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRoomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
